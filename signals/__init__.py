e8fb5764b941142eaeee391886e5ded9340370ee
Robert Asibor <rasibor@gmail.com>
2026-01-30 10:58:10 -0600

    Register VWAP mean reversion signal in signals package

--- signals/__init__.py
+++ signals/__init__.py
@@ -1,1 +1,7 @@
-# REA Capital – Trading Engine: signals package
+# REA Capital - Trading Engine: signals package
+
+from .vwap_mean_reversion import generate_vwap_mean_reversion_signals
+
+__all__ = [
+    "generate_vwap_mean_reversion_signals",
+]
